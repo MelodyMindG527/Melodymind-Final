706214322bed9680c457d5a1c1fb534e5a5a7866
abhi <abhishekbhang2006@gmail.com>
2025-10-05 10:38:51 +0530

    atlas connected

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,7 +1,9 @@
 export const env = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/melodymind',
+  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/melodymind',
+MONGODB_URI : process.env.MONGODB_URI ||'mongodb+srv://abhi:abhi2006@cluster-1.k5fgx.mongodb.net/melodymind?retryWrites=true&w=majority',
+
   JWT_SECRET: process.env.JWT_SECRET || 'change-me-in-prod',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '7d',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3001',
